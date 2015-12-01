6981ada3682700e8e4b5dddf3520fbfc8189e2b6
Andreas Schmid <andreas.schmid@bd.so.ch>
2015-12-01 07:52:22 +0100

    Simplify dbusr and dbpwd solution

--- import_schemas.sh
+++ import_schemas.sh
@@ -9,7 +9,6 @@
 # Variables
 hostname=localhost
 dbname=geodb
-dbusr=$USER
 datausername=data_user
 
 
@@ -20,6 +19,10 @@ chown --recursive $USER:geodata_admin /geodata/interlismodels/
 
 
 
+# Define DB user and password
+dbusr=$USER
+dbpwd=$(awk -F ':' '/'$dbusr'/ {print $5}' .pgpass) # hack: fetch password from .pgpass
+
 # Create DB schema av_dm01avso24_transfer
 
 # Download INTERLIS model
@@ -29,7 +32,6 @@ unzip -d /geodata/interlismodels/ 254900.zip dm01avso24.ili
 rm 254900.zip
 
 # Create empty DB schema
-dbpwd=$(awk -F ':' '/'$dbusr'/ {print $5}' .pgpass) # hack: fetch password from .pgpass
 psql -h $hostname -d $dbname -U $dbusr -c "SET ROLE admin; CREATE SCHEMA av_dm01avso24_transfer; GRANT USAGE ON SCHEMA av_dm01avso24_transfer TO $datausername, $dbusr; GRANT CREATE ON SCHEMA av_dm01avso24_transfer TO $dbusr;" # GRANT CREATE ... TO $dbusr is a workaround, as with ili2pg no SET ROLE admin is possible
 # Import INTERLIS model
 java -jar ili2pg-2.3.0/ili2pg.jar --schemaimport --dbhost $hostname --dbdatabase $dbname --dbschema av_dm01avso24_transfer --dbusr $dbusr --dbpwd $dbpwd --createscript create_av_dm01avso24_transfer.sql --createGeomIdx --createSingleEnumTab --t_id_Name ogc_fid --nameByTopic --importTid --log create_av_dm01avso24_transfer.log --modeldir /geodata/interlismodels/ --models DM01AVSO24 # and maybe: --createFk --createFkIdx --createBasketCol
@@ -53,7 +55,6 @@ unzip -d /geodata/interlismodels/ mopublicili1env1.3.zip MOpublic03_ili1_v1.3.il
 # rm mopublicili1env1.3.zip
 
 # Create empty DB schema
-dbpwd=$(awk -F ':' '/'$dbusr'/ {print $5}' .pgpass) # hack: fetch password from .pgpass
 psql -h $hostname -d $dbname -U $dbusr -c "SET ROLE admin; CREATE SCHEMA av_mopublic13_transfer; GRANT USAGE ON SCHEMA av_mopublic13_transfer TO $datausername, $dbusr; GRANT CREATE ON SCHEMA av_mopublic13_transfer TO $dbusr;" # GRANT CREATE ... TO $dbusr is a workaround, as with ili2pg no SET ROLE admin is possible
 # Import INTERLIS models
 java -jar ili2pg-2.3.0/ili2pg.jar --schemaimport --dbhost $hostname --dbdatabase $dbname --dbschema av_mopublic13_transfer --dbusr $dbusr --dbpwd $dbpwd --createscript create_av_mopublic13_transfer.sql --createGeomIdx --createEnumColAsItfCode --createSingleEnumTab --t_id_Name ogc_fid --nameByTopic --importTid --log create_av_mopublic13_transfer.log --modeldir /geodata/interlismodels/ --models 'MOpublic03_ili1_v13;LookUp_ili1_v13' # --createEnumColAsItfCode is rather not necessary
@@ -69,14 +70,13 @@ psql -h $hostname -d $dbname -U $dbusr -c "SET ROLE admin; REVOKE CREATE ON SCHE
 
 
 # Create DB schema work_full01
-exit 0 # TODO: disabled for now
+
 # Download INTERLIS model
 curl -O --remote-time https://raw.githubusercontent.com/schmandr/gdiserver/work/dmwork_full01.ili
 mv dmwork_full01.ili /geodata/interlismodels/
 # TODO: chown, chmod
 
 # Create empty DB schema
-dbpwd=$(awk -F ':' '/'$dbusr'/ {print $5}' .pgpass) # hack: fetch password from .pgpass
 psql -h $hostname -d $dbname -U $dbusr -c "SET ROLE admin; CREATE SCHEMA av_work_full01; GRANT USAGE ON SCHEMA av_work_full01 TO $datausername, $dbusr; GRANT CREATE ON SCHEMA av_work_full01 TO $dbusr;" # GRANT CREATE ... TO $dbusr is a workaround, as with ili2pg no SET ROLE admin is possible
 java -jar ili2pg-2.3.0/ili2pg.jar --schemaimport --dbhost $hostname --dbdatabase $dbname --dbschema av_work_full01 --dbusr $dbusr --dbpwd $dbpwd --createscript create_av_work_full01.sql --createGeomIdx --createSingleEnumTab --t_id_Name ogc_fid --nameByTopic --importTid --log create_av_work_full01.log --modeldir /geodata/interlismodels/ --models dmwork_full01.ili
 
